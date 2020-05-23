99a28b0c558def16749c46749d7d4efd7e93850e
Jake Ben-Tovim <jakebt@mahigaming.com>
2020-05-23 15:43:29 -0700

    Store assetName in DisplayComponent and move load handling to load()
    Makes it work better with subclasses that need to handle loading behavior less generically

--- src/engine/components/display/displayComponent.ts
+++ src/engine/components/display/displayComponent.ts
@@ -2,9 +2,15 @@ import { Renderable } from "../../core/renderable";
 import { AssetLoader } from "../../core/assetLoader";
 
 /**
- * The base class for display components which can be attached to an {@link Actor}.
+ * The base class for display components which can be attached to a {@link Actor}.
+ * Subclasses should override {@link DisplayComponent.load} to specify their load behavior.
  */
 export abstract class DisplayComponent<T extends PIXI.Container> extends Renderable {
+
+    /**
+     * The name of the asset associated with this display component.
+     */
+    protected _assetName: string;
 
     /**
      * The asset data associated with this diplay component.
@@ -17,7 +23,16 @@ export abstract class DisplayComponent<T extends PIXI.Container> extends Rendera
      */
     public constructor(assetName: string) {
         super();
-        this._internalAssetData = AssetLoader.getAsset<T>(assetName);
+        this._assetName = assetName;
+        this.load();
+    }
+
+    /**
+     * Loads this display object. The default behavior is to try to retrieve the asset with our
+     * assetName from the main asset loader, but subclasses should override as necessary.
+     */
+    public load(): void {
+        this._internalAssetData = AssetLoader.getAsset<T>(this._assetName);
         this.container.addChild(this._internalAssetData);
     }
 
