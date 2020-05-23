f4e5ded7e11eb63718e968454a8f1d7d6cc5d283
Jake Ben-Tovim <jakebt@mahigaming.com>
2020-05-23 14:48:40 -0700

    Set up test assets & gameConfig and test out laying out a scene

--- src/game/adVentureCapitalist.ts
+++ src/game/adVentureCapitalist.ts
@@ -1,15 +1,33 @@
 import { Game } from "../engine/core/game";
+import { Engine } from "../engine/core/engine";
+import { Level } from "../engine/core/level";
+import { AssetLoader } from "../engine/core/assetLoader";
+import { SpriteComponent } from "../engine/components/display/spriteComponent";
 
 /**
  * A recreation of the popular AdVenture Capitalist web game.
  */
 export class AdVentureCapitalist extends Game {
 
-    /**
-     * Callback for when the game is loaded.
-     * @param gameData The game's configured data.
-     */
-    public onGameLoad(gameData: any): void {
+    private _level: Level;
 
+    public constructor(gameData: any) {
+        super(gameData);
+        this._level = new Level("GameArea", true);
+
+        for (let i = 0; i < gameData.buyables.length; ++i) {
+            const buyable = gameData.buyables[i];
+            const spriteComponent = new SpriteComponent(buyable.image);
+            spriteComponent.transform.position.x = 250;
+            spriteComponent.transform.position.y = 250 + (i * 100);
+            this._level.root.addDisplayComponent(spriteComponent);
+        }
+    }
+
+    /**
+     * Performs update routines on the game.
+     * @param deltaTime The time in seconds since the last update tick.
+     */
+    public update(deltaTime: number): void {
     }
 }
