d88593a69152552cf2bc0abb1a089e1748cd8118
Jake Ben-Tovim <jacobbentovim@gmail.com>
2020-05-24 15:05:47 -0700

    Add support for different text formatting modes for text components

--- src/engine/components/display/textComponent.ts
+++ src/engine/components/display/textComponent.ts
@@ -1,11 +1,26 @@
 import { DisplayComponent } from "./displayComponent";
 import { Observable } from "../../core/observable";
-import { Dictionary } from "../../core/types";
 
 /**
  * The data shape that gets passed into the display component constructor for configuration purposes.
  */
-type TextComponentData = { text: string, format?: string, style?: any }
+type TextComponentData = { text: string | Observable<number> | Observable<string>, format?: TextFormatMode, style?: any }
+
+/**
+ * Represents the ways in which a text component's text can be formatted.
+ */
+export enum TextFormatMode {
+
+    /**
+     * (Default) The text is treated as raw text and no formatting is applied.
+     */
+    TEXT = "text",
+
+    /**
+     * The text will be formatted as a currency. This currently only supports USD.
+     */
+    CURRENCY = "currency"
+}
 
 /**
  * Represents a base text element.
@@ -19,10 +34,30 @@ export class TextComponent extends DisplayComponent<PIXI.Text, TextComponentData
     private _attachedObservable?: Observable<number> | Observable<string>;
 
     /**
+     * The currency formatter which text component used when their text format is set to {@link TextFormatMode.CURRENCY}.
+     */
+    public static currencyFormatter: Intl.NumberFormat = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" });
+
+    /**
      * Loads the text element.
      */
     public load(): void {
-        this._internalAssetData = new PIXI.Text(this._assetData.text, this._assetData.style);
+
+        // Default the format mode to text.
+        if (this._assetData.format === undefined) {
+            this._assetData.format = TextFormatMode.TEXT;
+        }
+
+        // Default the starting text to an empty string if the text was set to watch an observable.
+        const isObservable = (this._assetData.text instanceof Observable);
+        let startingText: string = (isObservable ? "" : String(this._assetData.text));
+        this._internalAssetData = new PIXI.Text(startingText, this._assetData.style);
+
+        // Attach to the configured observable if applicable.
+        if (isObservable) {
+            this.attachTo(this._assetData.text as (Observable<string> | Observable<number>));
+        }
+
         this.container.addChild(this._internalAssetData);
     }
 
@@ -30,8 +65,17 @@ export class TextComponent extends DisplayComponent<PIXI.Text, TextComponentData
      * Updates the text this component is displaying.
      * @param newText The new text value to display.
      */
-    public setText(newText: string): void {
-        this._internalAssetData.text = newText;
+    public setText(newText: string | number): void {
+        switch (this._assetData.format) {
+            case TextFormatMode.CURRENCY:
+                newText = TextComponent.currencyFormatter.format(Number(newText));
+                break;
+            default:
+                newText = String(newText);
+                break;
+        }
+
+        this._internalAssetData.text = String(newText);
     }
 
     /**
